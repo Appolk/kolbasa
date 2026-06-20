c5fd1bdbd66db57daf9272934cb12187b3a8b111
Appo <322@322.322>
2026-06-21 01:27:16 +0300

    322

---
+++ target/debug/deps/kolbasa-19a23403bca64407.d
@@ -0,0 +1,5 @@
+C:\Users\322\Documents\rust\kolbasa\target\debug\deps\kolbasa-19a23403bca64407.d: src\main.rs
+
+C:\Users\322\Documents\rust\kolbasa\target\debug\deps\libkolbasa-19a23403bca64407.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/kolbasa-ae20a9134c006efc.d
@@ -0,0 +1,5 @@
+C:\Users\322\Documents\rust\kolbasa\target\debug\deps\kolbasa-ae20a9134c006efc.d: src\main.rs
+
+C:\Users\322\Documents\rust\kolbasa\target\debug\deps\libkolbasa-ae20a9134c006efc.rmeta: src\main.rs
+
+src\main.rs:
